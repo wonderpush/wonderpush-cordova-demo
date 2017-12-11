3b360837624904cf196c037d7e6a6f8e76f15d33
Olivier Favre <olivier@wonderpush.com>
2017-12-11 11:21:51 +0100

    WIP

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+	cordova.plugins.WonderPush.initialize();
     },
     // Bind Event Listeners
     //
