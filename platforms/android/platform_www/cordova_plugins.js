7d4cb512bafb853ccb991e8395ac5bbb3dda8ec4
Olivier Favre <olivier@wonderpush.com>
2017-12-15 18:38:37 +0100

    Re-add android platform + commit Android files

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "wonderpush-cordova-sdk.WonderPush",
+    "file": "plugins/wonderpush-cordova-sdk/www/WonderPush.js",
+    "pluginId": "wonderpush-cordova-sdk",
+    "clobbers": [
+      "cordova.plugins.WonderPush"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "wonderpush-cordova-sdk": "0.1.0",
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
